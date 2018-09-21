dff88169260922c00c99726314f7c46775aa7803
mikrohash.de <iota@mikrohash.de>
2018-09-21 17:50:14 +0200

    fix: wrong qlweb version

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static final DateFormat DF = new SimpleDateFormat("YYYY/MMM/dd HH:mm:ss");
     public static final String VERSION = "0.5.0";
-    public static final String QLWEB_VERSION = "0.5.0";
+    public static final String QLWEB_VERSION = "0.5.1";
     private static final Scanner s = new Scanner(System.in);
 
     private static Persistence persistence;
